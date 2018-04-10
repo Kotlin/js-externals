54848a1b000f87f499f576f3c5d59c080dcbd3a9
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2018-04-10 21:11:26 +0300

    Add packages

--- externals/firebase-admin/v1/src/index.admin.app.kt
+++ externals/firebase-admin/v1/src/index.admin.app.kt
@@ -1,6 +1,6 @@
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 @file:JsQualifier("admin.app")
-package admin.app
+package package js.externals.firebase.admin.app
 
 import kotlin.js.*
 import kotlin.js.Json
--- externals/firebase-admin/v1/src/index.admin.auth.kt
+++ externals/firebase-admin/v1/src/index.admin.auth.kt
@@ -1,6 +1,6 @@
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 @file:JsQualifier("admin.auth")
-package admin.auth
+package js.externals.firebase.admin.auth
 
 import kotlin.js.*
 import kotlin.js.Json
