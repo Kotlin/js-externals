73a8698e52e3dc5c7a801f9e11857013eda9c477
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2018-04-13 23:16:30 +0300

    Fix wrong package declaration

--- externals/firebase-admin/v5/src/index.admin.app.kt
+++ externals/firebase-admin/v5/src/index.admin.app.kt
@@ -10,7 +10,7 @@
 
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 @file:JsQualifier("admin.app")
-package package js.externals.firebase.admin.app
+package js.externals.firebase.admin.app
 
 import kotlin.js.*
 import kotlin.js.Json
