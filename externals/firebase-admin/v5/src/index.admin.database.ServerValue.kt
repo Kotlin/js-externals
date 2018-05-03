5bb10323d87a8486ae5fbd70a522af5dc38d9fff
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2018-05-03 23:17:17 +0300

    Add missing annotations on firebase-admin files

--- externals/firebase-admin/v5/src/index.admin.database.ServerValue.kt
+++ externals/firebase-admin/v5/src/index.admin.database.ServerValue.kt
@@ -10,6 +10,8 @@
 
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 @file:JsQualifier("admin.database.ServerValue")
+@file:JsNonModule
+@file:JsModule("firebase-admin")
 package js.externals.firebase.admin.database.ServerValue
 
 import kotlin.js.*
--- externals/firebase-admin/v5/src/index.admin.instanceId.kt
+++ externals/firebase-admin/v5/src/index.admin.instanceId.kt
@@ -10,6 +10,8 @@
 
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 @file:JsQualifier("admin.instanceId")
+@file:JsNonModule
+@file:JsModule("firebase-admin")
 package js.externals.firebase.admin.instanceId
 
 import kotlin.js.*
